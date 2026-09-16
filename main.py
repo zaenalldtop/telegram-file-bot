5dcff6866bcd485cfcf4a740a808cdf47c55e912
zaenalldtop <62002815+zaenalldtop@users.noreply.github.com>
2026-09-16 15:24:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 
 API_ID = int(os.environ.get("API_ID", "39206186").strip()) 
 API_HASH = os.environ.get("API_HASH", "f1f40463bd79b121b4bff7a76c47ac16").strip()
-BOT_TOKEN = os.environ.get("BOT_TOKEN").strip()
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8913324140:AAFt0rHCeNPScTHThGPPmIR4S2cdLiZfMw4").strip()
 
-CUSTOM_DOMAIN = "https://cloud.primadigitalprint.com"
+CUSTOM_DOMAIN = "https://cloud.primadigitalprint.com/d"
 
 app = Client("file_to_link_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
@@ -18,7 +18,7 @@ async def generate_link(client, message):
         else:
             file_id = getattr(message, message.media.value).file_id
             
-        # Membuat format tautan kustom menggunakan domain Anda
+        # Membuat format tautan kustom menggunakan gabungan domain dan ID file unik
         download_link = f"{CUSTOM_DOMAIN}/{file_id}"
         
         # Mengirimkan pesan baru secara langsung tanpa sistem edit teks loading
@@ -33,3 +33,4 @@ async def generate_link(client, message):
 if __name__ == "__main__":
     print("--- BOT SERVER TELEGRAM ANTI-BENTROK AKTIF ---")
     app.run()
+
