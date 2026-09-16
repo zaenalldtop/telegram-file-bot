117bc0d3bb2553aa3cb351f3d40f8bc1d947bea6
zaenalldtop <62002815+zaenalldtop@users.noreply.github.com>
2026-09-16 20:54:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ async def generate_link(client, message):
             ])
             
             await message.reply_text(
-                f"✅ **FILE SUKSES TERUNGGAH!**\n\n"
-                f"📁 **Nama Berkas:** `{file_name}`\n\n"
+                f"✅ FILE SUKSES TERUNGGAH!\n\n"
+                f"📁 Nama Berkas: `{file_name}`\n\n"
                 f"__Tautan resmi Penyimpanan Berkas Prima Digital Print.__"
                 reply_markup=tombol_salin
             )
